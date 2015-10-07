ca1114c47f1494815afdc2e7fc14da9790f1e3fd
pierre <pierre@snowcap.be>
2015-10-07 15:31:58 +0200

    Fixed setup.py homepage

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pvanliefland/kelly',
+    url='https://github.com/youhaveajack/kelly',
 
     # Author details
     author='Pierre Vanliefland',
